e2cff88d802f636fbeba139a2cf452ff7423e605
Wei Gao <jky239@gmail.com>
2013-10-02 13:33:01 +0800

    Increase timeout settings for test as the network is not good in CI

--- test/test2.js
+++ test/test2.js
@@ -6,7 +6,7 @@ var assert = require('chai').assert,
 require('es6-map-shim');
 
 describe('hessian 2.0 test', function() {
-    this.timeout(5000);
+    this.timeout(10000);
     var proxy;
     before(function() {
         proxy = new Proxy('http://hessian.caucho.com/test/test2');
